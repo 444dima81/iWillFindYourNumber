8704673ea107b2adaa7c16c2d93f6ccb849d67b4
Dmitriy Koshelev <dmitriykoahelev@icloud.com>
2024-05-19 18:15:21 +0300

    first commit

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,6 +1,8 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+// Окно для ввода
+let minValue = parseInt(prompt('Минимальное знание числа для игры','-1000'));
+let maxValue = parseInt(prompt('Максимальное знание числа для игры','1000'));
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
@@ -11,18 +13,38 @@ const answerField = document.getElementById('answerField');
 orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
+// Заново
 document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
-})
+        answerField.innerText = alert('Давай еще раз?');
+        minValue = parseInt(prompt('Минимальное знание числа для игры','-1000'));
+        maxValue = parseInt(prompt('Максимальное знание числа для игры','1000'));
+        alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+        if (Number.isNaN(minValue)){
+            minValue = -1000 === 'default';
+            console.log(minValue);
+        } 
+        if (Number.isNaN(maxValue)){
+            maxValue = 1000 === 'default';
+            console.log(maxValue);
+        } 
 
+        answerNumber  = Math.floor((minValue + maxValue) / 2);
+        answerField.innerText = `Вы загадали число ${answerNumber }?`
+        orderNumber = '0';
+        orderNumberField.innerText = orderNumber;
+        gameRun = true;
+    })
+    // смотреть ошибку в заново и вводе nswerNumber orderNumber
+
+
+// Больше
 document.getElementById('btnOver').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
             const phraseRandom = Math.round( Math.random());
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
+                `Что то пошло не так!\n\u{1F631}` ;
                 `Я сдаюсь..\n\u{1F92F}`;
 
             answerField.innerText = answerPhrase;
@@ -33,14 +55,45 @@ document.getElementById('btnOver').addEventListener('click', function () {
             orderNumber++;
             orderNumberField.innerText = orderNumber;
             answerField.innerText = `Вы загадали число ${answerNumber }?`;
-        }
+        } 
     }
 })
 
+
+// Меньше
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Что то пошло не так!\n\u{1F631}` ;
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber  + 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        } 
+    }
+})
+
+// Верно
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        const phraseRandom = Math.round( Math.random());
+        const answerPhrase = (phraseRandom === 1) ?
+        `ТЫ молодец!\n\u{1F60D}` :
+        `Победа!\n\u{1F911}` ;
+        `УРАААА ТЫ КРУТ!\n\u{1F60E}`;
+        answerField.innerText = answerPhrase;
+        
         gameRun = false;
+        orderNumber = '0';
+        orderNumberField.innerText = orderNumber;
     }
 })
-
